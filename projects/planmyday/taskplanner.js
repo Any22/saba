8d41d904ee9c24faf08274375d05aea8b119cd7b
Saba Akhtar <sabasaeedakhtar@gmail.com>
2021-02-07 21:56:29 +1100

    added date validation

--- projects/planmyday/taskplanner.js
+++ projects/planmyday/taskplanner.js
@@ -104,7 +104,7 @@ class Card {
 
                 displayTask(){
                    this.master.innerHTML ="";                                           //refreshing page content 
-                  const mynewtasks= JSON.parse(localStorage.getItem ("mytasks"));   
+                  let mynewtasks= JSON.parse(localStorage.getItem ("mytasks"));   
                    this.cardArr=mynewtasks;
                 
                 for (let i=0 ; i<mynewtasks.length ; i++){
@@ -154,11 +154,13 @@ class Card {
         const taskcontainer = document.querySelector("#taskcontainer"); 
         const cardDeck=new CardManager(taskcontainer); 
         const allTask=document.querySelector("#allTasks");
+
+
         allTask.addEventListener("click",displayallTasks);
         function displayallTasks(){
           cardDeck.displayTask();
         }
-        //adding tasks
+        //taking inputs for adding the tasks
         let tname    = document.querySelector("#text1");               
         let tdes     = document.querySelector("#des"); 
         let assignee = document.querySelector("#assignee");
@@ -169,85 +171,102 @@ class Card {
         let nmErrMsg = document.querySelector("#nmErrMsg"); 
         let nmErrMsg1 = document.querySelector("#nmErrMsg1");
         let nmErrMsg2 = document.querySelector("#nmErrMsg2");
-        addButton.onclick= function(){
-        
-        let validStatus ;
-      
-        if ( tname.value == "" || tname.value.length < 5)
-           {   
-              
-              nmErrMsg.innerHTML="*Please ,fill this field with atleast 8 characters";
-              nmErrMsg.style.color="red";
-              tname.style.borderColor = "red";
-              tname.focus();
-              validStatus = false;
-              
-           } else 
-           {
-             
-              validStatus = true;
-              
-           }
+        let checkvalidDate = false;
+        let validname ,validdes,validassignee;
      
-        if (tdes.value == "" || tdes.value.length < 10)                               
-           
-           { 
-              nmErrMsg1.innerHTML= "*Please ,fill this field with atleast 15 characters";
-              nmErrMsg1.style.color="red";
-              tdes.style.borderColor = "red"; 
-              tdes.focus(); 
-              validStatus= false; 
-           
-           } else 
-           {
-              
-              tdes.style.borderColor = "green";
-              nmErrMsg1.innerHTML="looks good";
-              nmErrMsg1.style.color="green";
-              validStatus = true;
-           }
-            
-           if (assignee.value == "" || assignee.value.length < 5)                               
-               
-           { 
-            nmErrMsg2.innerHTML="*Please ,fill this field with atleast 8 characsters";
-            nmErrMsg2.style.color="red";
-            assignee.style.borderColor = "red"; 
-            assignee.focus(); 
-            validStatus= false; 
-            //  break;
-           } 
-           else 
-           {
-                  
-              assignee.style.borderColor = "green";
-              nmErrMsg2.innerHTML="looks good";
-              nmErrMsg2.style.color="green";
-              validStatus = true;
-           }
+// Adding the task to the object array if valiadtion completes 
+addButton.onclick = function(){
+  datevalidation(dDate,nmErrMsg3);                                              //calling function to validate the date 
+   
+  if (validname && validdes && validassignee && checkvalidDate){
+      cardDeck.addcard(tname.value,tdes.value,assignee.value,dDate.value,sTatus.value);
+      cardDeck.displayTask();
+       $("#myModal").modal("hide");
+      resetFields();   
+   }
+else
+{
+   alert("Fill all mandatory fields correctly");
+}
+}
+
+// Validation of all mandatory fields 
+tname.addEventListener("input",function(event)
+{
+   if (event.target.value && event.target.value.length <=8){
+         nmErrMsg.innerHTML="Please ,fill this field with atleast 8 characters";
+         nmErrMsg.style.color="red";
+         tname.style.borderColor = "red";
+         tname.focus();
+         validname = false;
+         // break;
+      } else 
+      {
+        nmErrMsg.innerHTML="looks ok!";
+        nmErrMsg.style.color="green";
+        tname.style.borderColor = "green";
+        validname=true;
+      }
+   }
+);
 
 
-             if (validStatus==true)
-             {
-                nmErrMsg.innerHTML="looks good";
-                nmErrMsg.style.color="green";
-                tname.style.borderColor = "green";
-                
-                cardDeck.addcard(tname.value,tdes.value,assignee.value,dDate.value,sTatus.value);
-                // cardDeck.displayListHtml();
-                 cardDeck.displayTask();
-                setTimeout(function(){
-                $("#myModal").modal("hide");
-                resetFields();
-             }, 900);
-                
+tdes.addEventListener("input",function(event){
+   if (event.target.value && event.target.value.length <= 15) {
+      nmErrMsg1.innerHTML="*Please ,fill this field with atleast 15 characters";
+      nmErrMsg1.style.color="red";
+      des.style.borderColor = "red"; 
+      des.focus();
+      validdes=false; 
+   } else{
+       des.style.borderColor = "green";
+       nmErrMsg1.innerHTML="looks good";
+       nmErrMsg1.style.color="green";
+       validdes=true;
+         
+   } 
+});
+
+assignee.addEventListener("input",function(event){
+   if (event.target.value && event.target.value.length <= 8) { 
+        nmErrMsg2.innerHTML="Please ,fill this field with atleast 8 characsters";
+        nmErrMsg2.style.color="red";
+        assignee.style.borderColor = "red"; 
+        assignee.focus(); 
+        validassignee=false;
+        } 
+        else 
+        {
                
-             }else 
-             {
-               alert("Please, fill the mandatory fields");
-             }
-       }
+        assignee.style.borderColor = "green";
+        nmErrMsg2.innerHTML="looks good";
+        nmErrMsg2.style.color="green";
+        validassignee=true;
+        }
+});  
+//Date Validation to restrict the user to choose a date from today's date or onwards 
+// date is not a mandatory field , the user may leave it empty 
+
+function datevalidation (dateElement,errorElement){
+       var currentDate= new Date();
+       let dueDate = new Date(dateElement.value);
+       if (dueDate ==""){
+        dueDate.value = new Date().toISOString().slice(0,10);
+        checkValidDate = true;
         
+      }
+      else if (dueDate < currentDate){
+        errorElement.innerHTML = "*Please choose a date from today";
+        errorElement.style.color = "red";
+        dateElement.focus();
+        checkValidDate = false; 
+     } 
+      else if (dueDate >= currentDate){
+        errorElement.innerHTML= " "
+        checkvalidDate=true; 
+      }
+  }      
+    //  Refreshing the fields for new entries
     function resetFields(){
         tname.value     = null;
         tdes.value      = null;
@@ -259,11 +278,12 @@ class Card {
         nmErrMsg2.innerHTML="";
         tname.style.borderColor = "lightgrey";
         tdes.style.borderColor = "lightgrey"; 
-        assignee.style.borderColor = "lightgrey"; 
-        
+        assignee.style.borderColor = "lightgrey";  
     }
+    
+
+    // editing the fields 
     function edifunc(event){
-      
       let taskElement = event.target.closest(".Edit");                       
       let edtIdArr = taskElement.id.split("_");                                         //spliting the id by underscore. i.e . dbuton_id 
       const retreiveId = edtIdArr[1];
@@ -294,19 +314,18 @@ class Card {
       let tempdueDate = document.querySelector("#edDate").value;
       let tempstatus = document.querySelector("#esTatus").value;  
       cardDeck.updateTask(tempId, tempname,tempdesc,tempassign,tempdueDate,tempstatus);
-      // cardDeck.displayListHtml();
-       cardDeck.displayTask();
+      cardDeck.displayTask();
       $('#ediTModal').modal('hide');
     }
 
+      // deleting the particular record 
        function delfunc(event){
         let taskElement = event.target.closest(".delete");                      
         let delIdArr = taskElement.id.split("_");                              
         let retreiveId = delIdArr[1];
         // alert(retreiveId);
         cardDeck.deletFunc(retreiveId);
-        // cardDeck.displayListHtml();
-         cardDeck.displayTask();
+        cardDeck.displayTask();
         }
  
         // displaying by status
