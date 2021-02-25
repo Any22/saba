e232c72e7c6a4694c9240ff27e0e8b1626b09122
Saba Akhtar <sabasaeedakhtar@gmail.com>
2021-02-25 11:20:06 +1100

    adjusted the size of project display cards

--- portfolio.js
+++ portfolio.js
@@ -1,23 +1,12 @@
-// menu burger button 
-// let menuOpen=false;
-// let menuBtn=document.querySelector("#check");
-// menuBtn.addEventListener('click',() => {
-//     if (!menuOpen){
-//         menuBtn.classList.add('open');
-//         menuOpen=true;
-//     } else{
-//         menuBtn.classList.remove('open');
-//         menuOpen=false;
-//     }
-// })
 
 
-//show text less and more 
+//code behind button to show more/less text in About me div
 let button1=document.querySelector("#button1");
 button1.onclick = function(){ 
  $("#lshow").toggle();
  }
 
+// slides in my tech stack div 
  let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -47,7 +36,8 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-//Get the button:
+
+//code behind the button to scroll to the top
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -65,4 +55,114 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+//Modal to show project details 
+const modal = document.getElementById("myModal");
+const modal1 = document.getElementById("myModal1");
+const modal2 = document.getElementById("myModal2");
+const modal3 = document.getElementById("myModal3");
+const modal4 = document.getElementById("myModal4");
+const modal5 = document.getElementById("myModal5");
+
+// "Details" button that opens the modal
+const btn = document.getElementById("detailBtn");
+const btn1 = document.getElementById("detailBtn1");
+const btn2 = document.getElementById("detailBtn2");
+const btn3 = document.getElementById("detailBtn3");
+const btn4 = document.getElementById("detailBtn4");
+const btn5 = document.getElementById("detailBtn5");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+const span1 = document.getElementsByClassName("close1")[0];
+const span2 = document.getElementsByClassName("close2")[0];
+const span3 = document.getElementsByClassName("close3")[0];
+const span4 = document.getElementsByClassName("close4")[0];
+const span5 = document.getElementsByClassName("close5")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+
+btn4.onclick = function() {
+  modal4.style.display = "block";
+}
+
+btn5.onclick = function() {
+  modal5.style.display = "block";
+}
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
+
+span5.onclick = function() {
+  modal5.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == modal5) {
+    modal5.style.display = "none";
+  }
 }
